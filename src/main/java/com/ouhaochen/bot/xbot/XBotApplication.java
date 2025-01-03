67614aa9a666f25609eb8de675ceac00905dc31e
zhouyinpeng <yinpengzhou@outlook.com>
2025-01-03 17:38:19 +0800

    Initial commit

---
+++ src/main/java/com/ouhaochen/bot/xbot/XBotApplication.java
@@ -0,0 +1,13 @@
+package com.ouhaochen.bot.xbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ouhaochen/bot/xbot/XBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouhaochen.bot.xbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
