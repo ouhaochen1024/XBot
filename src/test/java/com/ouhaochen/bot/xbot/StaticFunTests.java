1dad112534a95877d99bbf0b97aba0a663fd200f
ouhaochen <yinpengzhou@outlook.com>
2025-01-05 18:18:19 +0800

    fixed the test class could not be started

--- src/test/java/com/ouhaochen/bot/xbot/StaticFunTests.java
+++ src/test/java/com/ouhaochen/bot/xbot/StaticFunTests.java
@@ -1,12 +1,12 @@
-//package com.ouhaochen.bot.xbot;
-//
-//
-//import org.junit.jupiter.api.Test;
-//
-//public class StaticFunTests {
-//
-//    @Test
-//    public void test() {
-//
-//    }
-//}
+package com.ouhaochen.bot.xbot;
+
+
+import org.junit.jupiter.api.Test;
+
+public class StaticFunTests {
+
+    @Test
+    public void test() {
+
+    }
+}
--- src/test/java/com/ouhaochen/bot/xbot/XBotApplicationTests.java
+++ src/test/java/com/ouhaochen/bot/xbot/XBotApplicationTests.java
@@ -1,13 +1,14 @@
-//package com.ouhaochen.bot.xbot;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class XBotApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.ouhaochen.bot.xbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = XBotApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class XBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hello World!");
+	}
+
+}
