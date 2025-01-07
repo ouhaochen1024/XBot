1a937e23cccc454e848f2659eb8933cd57f41b1d
ouhaochen <yinpengzhou@outlook.com>
2025-01-07 17:03:09 +0800

    移动插件权限管理到BasePlugin

--- src/main/java/com/ouhaochen/bot/xbot/core/plugins/GroupManagePlugin.java
+++ src/main/java/com/ouhaochen/bot/xbot/core/plugins/GroupManagePlugin.java
@@ -27,62 +27,6 @@ public class GroupManagePlugin {
 
     private final BotGroupService botGroupService;
 
-    @Permission(checkGroup = false)
-    @GroupMessageHandler
-    @MessageHandlerFilter(cmd = "添加本群")
-    public void addGroup(Bot bot, GroupMessageEvent event) {
-        Long groupId = event.getGroupId();
-        // 判断是否已经添加
-        BotGroupEntity botGroupEntity = botGroupService.lambdaQuery().eq(BotGroupEntity::getGroupId, groupId).eq(BotGroupEntity::getBotId, event.getSelfId()).eq(BotGroupEntity::getDelFlag, DelFlagEnum.NOT_DELETED.getCode()).one();
-        if (null != botGroupEntity) {
-            bot.sendGroupMsg(event.getGroupId(), "该群已经添加过了", false);
-            return;
-        }
-        // 添加群
-        botGroupService.save(new BotGroupEntity().setGroupId(groupId).setBotId(bot.getSelfId()));
-        // 构建消息
-        String sendMsg = MsgUtils.builder().text(String.format("群%d已经添加成功", groupId)).build();
-        // 发送消息
-        bot.sendGroupMsg(event.getGroupId(), sendMsg, false);
-    }
-
-    @Permission(checkGroup = false)
-    @AnyMessageHandler
-    @MessageHandlerFilter(cmd = "^添加群\\s(.*)?$")
-    public void addGroup(Bot bot, AnyMessageEvent event, Matcher matcher) {
-        Long groupId = MatcherUtil.getLongStr(bot, event, matcher);
-        // 判断是否已经添加
-        BotGroupEntity botGroupEntity = botGroupService.lambdaQuery().eq(BotGroupEntity::getGroupId, groupId).eq(BotGroupEntity::getBotId, event.getSelfId()).eq(BotGroupEntity::getDelFlag, DelFlagEnum.NOT_DELETED.getCode()).one();
-        if (null != botGroupEntity) {
-            bot.sendMsg(event, "该群已经添加过了", false);
-            return;
-        }
-        // 添加群
-        botGroupService.save(new BotGroupEntity().setGroupId(groupId).setBotId(bot.getSelfId()));
-        // 构建消息
-        String sendMsg = MsgUtils.builder().text(String.format("群%d已经添加成功", groupId)).build();
-        // 发送消息
-        bot.sendMsg(event, sendMsg, false);
-    }
-
-    @Permission(checkGroup = false)
-    @AnyMessageHandler
-    @MessageHandlerFilter(cmd = "^删除群\\s(.*)?$")
-    public void delGroup(Bot bot, AnyMessageEvent event, Matcher matcher) {
-        Long groupId = MatcherUtil.getLongStr(bot, event, matcher);
-        BotGroupEntity botGroupEntity = botGroupService.lambdaQuery().eq(BotGroupEntity::getGroupId, groupId).eq(BotGroupEntity::getBotId, event.getSelfId()).eq(BotGroupEntity::getDelFlag, DelFlagEnum.NOT_DELETED.getCode()).one();
-        if (null != botGroupEntity) {
-            botGroupEntity.setDelFlag(DelFlagEnum.DELETED.getCode());
-            botGroupService.updateById(botGroupEntity);
-            // 构建消息
-            String sendMsg = MsgUtils.builder().text(String.format("群%d已经删除成功", groupId)).build();
-            // 发送消息
-            bot.sendMsg(event, sendMsg, false);
-        } else {
-            bot.sendMsg(event, "该群还未添加", false);
-        }
-    }
-
     @Permission(checkUser = false)
     @GroupAddRequestHandler
     public void handleAddGroup(Bot bot, GroupAddRequestEvent event){
